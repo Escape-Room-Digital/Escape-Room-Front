7b62fdb002443280664f2097fc7669a592c0ba0c
maximiliano <maxlobosc@gmail.com>
2022-09-21 00:21:20 +0200

    changes_title_options_and_sizebox

--- Escape-Room-Digital/src/views/PanelAdmin.vue
+++ Escape-Room-Digital/src/views/PanelAdmin.vue
@@ -5,27 +5,27 @@
 <template>
   <div class="container_admin_options">
     <div id="container_title">
-      <p id="title_admin">Panel de Administrador</p>
+      <p id="title_admin">Panel Administrador</p>
     </div>
 
     <div class="container_options">
       <div id="container_scaperooms">
-        <p class="title_options">Escape Rooms</p>
+        <p class="title_options"><strong>Escape Rooms</strong></p>
       </div>
 
       <div id="container_pruebasLogica">
-        <p  class="title_options">Pruebas de Logica</p>
+        <p  class="title_options"><strong>Pruebas de Logica</strong></p>
       </div>
 
       <div id="container_pruebasCodigo">
-        <p  class="title_options">Pruebas de Codigo</p>
+        <p  class="title_options"><strong>Pruebas de Codigo</strong></p>
       </div>
 
       <div id="container_listadoCandidatos">
-        <p  class="title_options">Listado de Candidatos</p>
+        <p  class="title_options"><strong>Listado de Candidatos</strong></p>
       </div>
       <div id="container_listadoCandidatosPruebaCompletada">
-        <p  class="title_options">Pruebas Completadas</p>
+        <p  class="title_options"><strong>Pruebas Completadas</strong></p>
       </div>
     </div>
   </div>
