3cff46b31cee9df17e81f18617ec245218e032b6
David <d.barcia9@gmail.com>
2022-09-22 12:31:33 +0200

    cambios

--- Escape-Room-Digital/src/views/HomeView.vue
+++ Escape-Room-Digital/src/views/HomeView.vue
@@ -1,3 +1,21 @@
 <template>
+  <div class="imagen">
   <div>Home</div>
+  </div>
 </template>
+
+<style scoped>
+  .imagen{
+    background-image: url("https://media-exp1.licdn.com/dms/image/C4E1BAQHWqn0FLbkJ7g/company-background_10000/0/1602242826197?e=2147483647&v=beta&t=sy2YtXB8GrNZsTWkOfzrRRGa9GRi5zfXQ5VSlF6Cc5s");
+    width: 100%;
+    height: 900px;
+    opacity: 20% ;
+    background-size: cover;
+    background-attachment: fixed;
+    margin:0;
+    padding: 0;
+    
+    
+
+}
+</style>
