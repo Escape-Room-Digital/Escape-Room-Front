173f381f8b164da55194dbe87e279407669b0419
Maximiliano Lobos <maxlobosc@gmail.com>
2022-09-27 09:25:29 +0200

    mergeReadTestFront

--- Escape-Room-Digital/src/views/TableLogicTest.vue
+++ Escape-Room-Digital/src/views/TableLogicTest.vue
@@ -1,42 +1,53 @@
+<script setup>
+import { useGetData } from "../services/prueba";
+
+
+
+const { getData, data, error, loading } = useGetData();
+
+getData("http://127.0.0.1:8000/api/logictest")
+
+
+</script>
+
+
 <template>
-    <v-table fluid>
-        <thead class="header-list-rooms" elevation="10" >       
-                <tr>
-                    <th>Nombre</th>     
-                    <th>Acciones</th>             
-                </tr>       
-        </thead>
-       
-            <tbody class="" elevation="10">
-                
+    
+    <p  class="" v-if="loading">Cargando información...</p>
+    <div class="alert alert-danger mt-2" v-if="error">{{ error }}</div>
+    <v-table v-if="data" elevation="10">
+        <thead class="header-list-rooms">
             <tr>
-                <th>Ordena Números de Mayor a Menor</th>
-                
-                <v-spacer></v-spacer>
-                <v-btn-group >
-                    <v-checkbox class="text-rigth" v-model="select"  color="orange darken-3" value="trues" hide-details></v-checkbox>
-                    <v-btn variant="text" icon="mdi-delete" color="orange darken-3"></v-btn>
-                    <v-btn variant="text" icon="mdi-pencil" color="dark"></v-btn>
-                </v-btn-group>
+                <th id="names">Nombre</th>
+                <th id="accions">Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="list-group">
+                <th id="name" v-for="listOfLogicalTest in data">{{listOfLogicalTest.name}} <v-btn-group>
+                        <v-checkbox class="text-rigth" v-model="select" color="orange darken-3" value="trues"
+                            hide-details></v-checkbox>
+                        <v-btn variant="text" icon="mdi-delete" color="orange darken-3"></v-btn>
+                        <v-btn variant="text" icon="mdi-pencil" color="dark"></v-btn>
+                    </v-btn-group>
+                </th>
 
             </tr>
-                
+
+
         </tbody>
-        
-       
+        <div class="mt-2">
+            <button :disabled="!data.previous" class="btn btn-success me-2" @click="getData(data.previous)">
+                Previous
+            </button>
+            <button :disabled="!data.next" class="btn btn-primary" @click="getData(data.next)">
+                Next
+            </button>
+        </div>
     </v-table>
 </template>
 
-<script>
 
-export default {
-    data() {
-        return {
-            select: ['trues'],
-        }
-    },
-}
-</script>
 
 
 <style scoped>
