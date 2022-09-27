7a7c0a03867ccef78a073bf99075b15915822edb
Maximiliano Lobos <maxlobosc@gmail.com>
2022-09-27 23:31:43 +0200

    processCrudTestLogical

--- Escape-Room-Digital/src/services/prueba.js
+++ Escape-Room-Digital/src/services/prueba.js
@@ -2,14 +2,14 @@ import axios from "axios";
 import { ref } from "vue";
 
 export const useGetData = () => {
-    const data = ref(null);
+    // const data = ref(null);
     const error = ref(null);
     const loading = ref(true);
 
-    const getData = async (url) => {
+    const getData = async () => {
         loading.value = true;
         try {
-            const res = await axios.get(url);
+            // const res = await axios.get("http://127.0.0.1:8000/api/logictest");
             data.value = res.data;
         } catch (e) {
             // console.log(e);
@@ -18,6 +18,7 @@ export const useGetData = () => {
             loading.value = false;
         }
     };
+
 
     return {
         getData,
--- Escape-Room-Digital/src/views/TableLogicTest.vue
+++ Escape-Room-Digital/src/views/TableLogicTest.vue
@@ -1,11 +1,95 @@
-<script setup>
-import { useGetData } from "../services/prueba";
+<script>
+import axios from "axios";
+import { ref } from "vue";
+export default {
+data(){
+    return {
+        data: [],
+        dialog:false,
+        operacion:'',
+        listOfLogicalTest:{
+            id:null,
+            name:"",
+            statement:"",
+            question:"",
+            result:"",
+            clue:"",
+            image:"",
+
+        }
+
+        
+    }
+},
+
+created() {
+    this.mostrar()
+},
+methods: {
+    mostrar() {
+        axios.get("http://127.0.0.1:8000/api/logictest/").then(response =>{
+            this.data = response.data;
+
+        });
+    },
+    crear(){
+        const parametros = {name: this.listOfLogicalTest.name, statement: this.listOfLogicalTest.statement, question: this.listOfLogicalTest.question,result: this.listOfLogicalTest.result, clue: this.listOfLogicalTest.clue, image: this.listOfLogicalTest.image,}
+        axios.post("http://127.0.0.1:8000/api/logictest/create",parametros).then(response =>{
+        
+        this.mostrar();
+    });
+    this.listOfLogicalTest.name = "";
+    this.listOfLogicalTest.statement = "";
+    this.listOfLogicalTest.question = "";
+    this.listOfLogicalTest.result = "";
+    this.listOfLogicalTest.clue = "";
+    this.listOfLogicalTest.image = "";
+},
+    editar(){
+        const parametros = { id: this.listOfLogicalTest.id, name: this.listOfLogicalTest.name, statement: this.listOfLogicalTest.statement, question: this.listOfLogicalTest.question,result: this.listOfLogicalTest.result, clue: this.listOfLogicalTest.clue, image: this.listOfLogicalTest.image,}
+        axios.patch("http://127.0.0.1:8000/api/logictest/update/{id}" + this.listOfLogicalTest.id ,parametros).then(response =>{
+            this.mostrar();
+        });
+    },
+
+    },
+    borrar(id){
+     axios.delete("http://127.0.0.1:8000/api/logictest/delete/").then(response =>{
+     this.mostrar();
+     });
+    },
+guardar(){
+    if(this.operacion == 'crear'){
+        this.crear();
+    }
+    if(this.operacion == 'editar'){
+        this.editar();
+}
+},
+fornmNuevo(){
+    this.operacion = 'crear';
+    this.operacion.name= "";
+    this.operacion.statement= "";
+    this.operacion.question= "";
+    this.operacion.result = "";
+    this.operacion.clue = "";
+    this.operacion.image = "";
+
+},
+formEditar(id, name, statement, question, result, clue, image){
+
+    this.listOfLogicalTest.id= id;
+    this.listOfLogicalTest.name= name;
+    this.listOfLogicalTest.statement= statement;
+    this.listOfLogicalTest.question= question;
+    this.listOfLogicalTest.result= result;
+    this.listOfLogicalTest.clue= clue;  
+    this.listOfLogicalTest.imagen= image;
+    this.operacion = 'editar'
+}
 
 
-
-const { getData, data, error, loading } = useGetData();
-
-getData("http://127.0.0.1:8000/api/logictest")
+}
 
 
 </script>
@@ -24,12 +108,33 @@ getData("http://127.0.0.1:8000/api/logictest")
         </thead>
         <tbody>
             <tr class="list-group">
-                <th id="name" v-for="listOfLogicalTest in data">{{listOfLogicalTest.name}} <v-btn-group>
+                <th id="name" v-for="listOfLogicalTest in data" :key="listOfLogicalTest.name"> 
+                    
+                    {{listOfLogicalTest.name}} 
+                    
+
+
+
+
+
+                    <v-btn-group>
                         <v-checkbox class="text-rigth" v-model="select" color="orange darken-3" value="trues"
                             hide-details></v-checkbox>
-                        <v-btn variant="text" icon="mdi-delete" color="orange darken-3"></v-btn>
-                        <v-btn variant="text" icon="mdi-pencil" color="dark"></v-btn>
+
+
+
+                        <v-btn variant="text" @click="borrar(id)"  icon="mdi-delete" color="orange darken-3"></v-btn>
+
+
+
+
+                        <v-btn variant="text"  @click="formEditar(listOfLogicalTest.id, listOfLogicalTest.name, listOfLogicalTest.statement, listOfLogicalTest.question, listOfLogicalTest.result, listOfLogicalTest.clue, listOfLogicalTest.image)" icon="mdi-pencil" color="dark"></v-btn>
+
                     </v-btn-group>
+
+
+
+
                 </th>
 
             </tr>
@@ -37,7 +142,7 @@ getData("http://127.0.0.1:8000/api/logictest")
 
         </tbody>
         <div class="mt-2">
-            <button :disabled="!data.previous" class="btn btn-success me-2" @click="getData(data.previous)">
+            <button :disabled="!data.previous" class="btn btn-success me-2" method="delete" @click="getData(data.previous)">
                 Previous
             </button>
             <button :disabled="!data.next" class="btn btn-primary" @click="getData(data.next)">
