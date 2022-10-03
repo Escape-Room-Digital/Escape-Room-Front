99008a5b1f095f60ba030dff6a01c6ecd3d76cdb
Maximiliano Lobos <maxlobosc@gmail.com>
2022-10-03 10:23:04 +0200

    Logincandidato

---
+++ Escape-Room-Digital/src/components/game.vue
@@ -0,0 +1,17 @@
+<script>
+    export default {
+        
+    }
+    </script>
+
+
+<template>
+    <div class="container_game">
+        <h1>nhjchcdh</h1>
+        
+    </div>
+</template>
+
+<style>
+    
+</style>
