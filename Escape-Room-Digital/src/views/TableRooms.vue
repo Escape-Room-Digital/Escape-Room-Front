886dac594e3117d5659fa5b207ec12f84f71867e
Maximiliano Lobos <maxlobosc@gmail.com>
2022-10-04 15:28:19 +0200

    botonSelectEscapeRoom

--- Escape-Room-Digital/src/views/TableRooms.vue
+++ Escape-Room-Digital/src/views/TableRooms.vue
@@ -1,85 +1,79 @@
 <script setup>
 //imports
-import { ref } from "vue"
-import {useGetDataEscapeRoom} from "../services/serviceEscapeRoom";
-
-
-
+import { ref } from "vue";
+import { useGetDataEscapeRoom } from "../services/serviceEscapeRoom";
+import { useRouter, useRoute } from "vue-router";
 const switch1 = ref(true);
+const route = useRoute();
+const router = useRouter();
+const togglePushId = () => { 
+    router.push("/tablelogictest");
+};
 
-const { getDataLogic, data, errors, loading,destroyLogic } = useGetDataEscapeRoom();
-    const select = ref(true);
-    const deletes = ref(true);
+const { getDataLogic, data, errors, loading, destroyLogic } =
+  useGetDataEscapeRoom();
+const select = ref(true);
+const deletes = ref(true);
 
-    const deleteCode = async (id) => {
-    if (!window.confirm("You sure?")) {
-        return;
-    }
-    await destroyLogic(id);
-    await getDataLogic();
+const deleteCode = async (id) => {
+  if (!window.confirm("You sure?")) {
+    return;
+  }
+  await destroyLogic(id);
+  await getDataLogic();
 };
 const listOfLogicalTest = getDataLogic();
-
 </script>
 
-
 <template>
-    <h1>listado escape rooms</h1>
- 
-    <div>
-      
-        <RouterLink to="/codeeditorcreate">
-            <div id="button_create">
-                <!-- <v-btn variant="text" icon="mdi-plus-thick" color="blue darken-3" class="mt-2"></v-btn> -->
-               
-                    <p id="text_create">Crear Escape Room</p>  
-            </div>
-            
-       
-        
-        </RouterLink>
- 
-</div>
-<v-table fluid>
+  <h1>listado escape rooms</h1>
+
+  <div>
+    <RouterLink to="/codeeditorcreate">
+      <div id="button_create">
+        <!-- <v-btn variant="text" icon="mdi-plus-thick" color="blue darken-3" class="mt-2"></v-btn> -->
+
+        <p id="text_create">Crear Escape Room</p>
+      </div>
+    </RouterLink>
+  </div>
+  <v-table fluid>
     <thead class="header-list-rooms" elevation="10">
-        <tr>
-            <th id="names">Nombre</th>
-            <th id="actions"></th>
-        </tr>
+      <tr>
+        <th id="names">Nombre</th>
+        <th id="actions"></th>
+      </tr>
     </thead>
     <tbody>
-
-        <tr class="list-group" id="name" v-for="(listOfLogicalTest, id) in data" :key="id">
-            <td>{{listOfLogicalTest.name}}</td>
-            <!-- <td>{{listOfLogicalTest.statement}}</td>
+      <tr
+        class="list-group"
+        id="name"
+        v-for="(listOfLogicalTest, id) in data"
+        :key="id"
+      >
+        <td>{{ listOfLogicalTest.name }}</td>
+        <!-- <td>{{listOfLogicalTest.statement}}</td>
             <td>{{listOfLogicalTest.result}}</td> -->
-            <div>
-                
-                    <v-btn    class="ma-2" color="orange" dark>Seleccionar</v-btn>
-                 
-                 
-                     <v-btn icon="mdi-alpha-x" class="delete btn btn-danger" color="red" @click="deleteCode(listOfLogicalTest.id)"></v-btn>
-                 
-                  
-                     
-                         <router-link :to="{ name: 'codeeditoredit', params: { id: listOfLogicalTest.id }}">
- 
-                             <v-btn  icon="mdi-pencil" color="darken-3" ></v-btn>
-                            
-                                    
-                                
- 
-                         </router-link>
-                   
-                 
+        <div>
+          <v-btn class="ma-2" color="orange" @click="togglePushId">Seleccionar</v-btn>
 
+          <v-btn
+            icon="mdi-alpha-x"
+            class="delete btn btn-danger"
+            color="red"
+            @click="deleteCode(listOfLogicalTest.id)"
+          ></v-btn>
 
-
-
-            </div>
-        
-        </tr>
-
+          <router-link
+            :to="{
+              name: 'codeeditoredit',
+              params: { id: listOfLogicalTest.id },
+            }"
+          >
+            <v-btn icon="mdi-pencil" color="darken-3"></v-btn>
+          </router-link>
+        </div>
+      </tr>
     </tbody>
     <!-- <div class="mt-2">
             <button :disabled="!data.previous" class="btn btn-success me-2" @click="getDataCoder(data.previous)">
@@ -89,48 +83,38 @@ const listOfLogicalTest = getDataLogic();
                 Next
             </button>
         </div>  -->
-</v-table>
+  </v-table>
 
-    <RouterLink to="/paneladmin">
-
-        <v-btn class="btn btn-success" > BACK </v-btn>
-        
-    
-    </RouterLink>
-
+  <RouterLink to="/paneladmin">
+    <v-btn class="btn btn-success"> BACK </v-btn>
+  </RouterLink>
 </template>
 
-
 <style scoped>
+.header-list-rooms {
+  background: rgba(255, 71, 2, 0.58);
+}
+tr {
+  width: 80vw;
+}
+svg {
+  margin-top: 1vh;
+}
 
-    .header-list-rooms {
-    
-        background: rgba(255, 71, 2, 0.58);
-    }
-    tr{
-        width:80vw;
-        
-        
-    }
-    svg{
-        margin-top: 1vh;
-    }
-    
-    .table {
-        border: 1px solid rgba(255, 71, 2, 0.58);
-    }
-    .list-group{
-        display: flex;
-        flex-direction:row ;
-        justify-content: space-between;
-        
-    }
-    #button_create{
-        display: flex;
-    }
-    #text_create{
-        margin-top: 2vh;
-        color: black;
-        font-size: 2vh;
-    }
+.table {
+  border: 1px solid rgba(255, 71, 2, 0.58);
+}
+.list-group {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+#button_create {
+  display: flex;
+}
+#text_create {
+  margin-top: 2vh;
+  color: black;
+  font-size: 2vh;
+}
 </style>
