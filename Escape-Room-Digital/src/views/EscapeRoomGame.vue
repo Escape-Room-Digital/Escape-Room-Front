67d4c96c0b6a7e3f0a8257639aa812fc00a1a84d
lucyalvarado4692 <104829949+lucyalvarado4692@users.noreply.github.com>
2022-10-12 19:00:32 +0200

    modal ready

--- Escape-Room-Digital/src/views/EscapeRoomGame.vue
+++ Escape-Room-Digital/src/views/EscapeRoomGame.vue
@@ -1,21 +1,21 @@
-<script setup >
-import { ref, onMounted, computed } from 'vue';
+<script setup>
+import { ref, onMounted, computed } from "vue";
 //import users store
-import { useEscapeStore } from "../stores/useEscapeGameStore"
+import { useEscapeStore } from "../stores/useEscapeGameStore";
+import ModalClue from "@/components/ModalClue.vue";
 // declare store variable
 const store = useEscapeStore();
 
 const { fetchEscapes } = useEscapeStore();
 
 const getEscapeGame = computed(() => {
-    return store.getEscapeGame
-})
+  return store.getEscapeGame;
+});
 const escapes = computed(() => {
-    return store.escapes
-})
+  return store.escapes;
+});
 
 fetchEscapes();
-
 
 ////////////
 
@@ -25,107 +25,92 @@ const numdos = ref(0);
 const arrayAdd = ref([]);
 const arrayCompare = ref([]);
 
-
 const add = () => {
-    arrayAdd.value.push(number1.value);
-    console.log(arrayAdd.value)
-}
-
-const add1 = () => {
-    arrayCompare.value.push(numdos.value);
-    console.log(arrayCompare.value)
-}
-
-const compareNumber = () => {
-    if (arrayAdd.value >= arrayCompare.value) {
-        console.log('correct number')
-    } else {
-        console.log('wrong number')
-    }
+  arrayAdd.value.push(number1.value);
+  console.log(arrayAdd.value);
 };
 
-const mostrarResultado = ref('resultado');
+const add1 = () => {
+  arrayCompare.value.push(numdos.value);
+  console.log(arrayCompare.value);
+};
 
+const compareNumber = () => {
+  if (arrayAdd.value >= arrayCompare.value) {
+    console.log("correct number");
+  } else {
+    console.log("wrong number");
+  }
+};
 
-
-
-
+const mostrarResultado = ref("resultado");
 </script>
 
-
-
 <template>
+  <div>
+    <div
+      v-for="(getterEscape, index) in escapes"
+      :key="index"
+      :getterEscape="getterEscape"
+    >
+      <!--  v-for="value in getterEscape" :key="value" -->
+      <v-card class="mx-auto my-12 backgroud-color rounded-lg" max-width="674">
+        <v-card-title class="text-center background-title">{{
+          getterEscape.name
+        }}</v-card-title>
 
-    <div>
-        <div v-for="(getterEscape, index)  in escapes" :key="index" :getterEscape="getterEscape">
-            <!--  v-for="value in getterEscape" :key="value" -->
+        <v-card-text>
+          <div class="my-4 text-subtitle-1 text-center">
+            <p class="text-center">{{ getterEscape.statement }}</p>
+          </div>
+        </v-card-text>
 
-
-            <v-card class="mx-auto my-12 backgroud-color rounded-lg" max-width="674">
-
-                <v-card-title class="text-center background-title">{{getterEscape.name}}</v-card-title>
-
-                <v-card-text>
-                    <div class="my-4 text-subtitle-1 text-center">
-                        <p class="text-center">{{getterEscape.statement}}</p>
-                    </div>
-                </v-card-text>
-
-                <v-img height="250" :src="getterEscape.image"></v-img>
-                <v-divider class="mx-4"></v-divider>
-                <div>
-                    <p class="text-center">{{getterEscape.clue}}</p>
-                </div>
-
-                <input type="checkbox" id="checkbox" color="orange" @click="add1">
-                <label for="checkbox">opcion a</label>
-                <br>
-                <input type="checkbox" id="checkbox" @click="add">
-                <label for="checkbox">opcion b</label>
-
-
-                <v-btn @click="compareNumber"></v-btn>
-                <label for="checkbox">resultado</label>
-                <!-- Modal -->
-                <!-- <v-btn icon="mdi-" color="orange" dark>
-                Pista
-            </v-btn> -->
-
-
-
-                <!-- Button trigger modal SHOW-->
-                <button id="button" type="button" data-bs-toggle="modal"
-                    :data-bs-target="'#showModal' + getterEscape.id">
-                    PISTA
-                </button>
-
-                <modal-clue :getterEscape="getterEscape" :id="getterEscape.id"></modal-clue>
+        <v-img height="250" :src="getterEscape.image"></v-img>
+        <v-divider class="mx-4"></v-divider>
+        <div>
+          <p class="text-center">{{ getterEscape.clue }}</p>
         </div>
-        </v-card>
 
+        <input type="checkbox" id="checkbox" color="orange" @click="add1" />
+        <label for="checkbox">opcion a</label>
+        <br />
+        <input type="checkbox" id="checkbox" @click="add" />
+        <label for="checkbox">opcion b</label>
+
+        <v-btn @click="compareNumber"></v-btn>
+        <label for="checkbox">resultado</label>
+        <!-- Modal -->
+        <!-- <v-btn icon="mdi-" color="orange" dark>
+                Pista
+                </v-btn> -->
+
+        <!-- Button trigger modal SHOW-->
+        <button
+          id="button"
+          type="button"
+          data-bs-toggle="modal"
+          :data-bs-target="'#showModal' + getterEscape.id"
+        >
+          PISTA
+        </button>
+      </v-card>
+      <modal-clue :id="getterEscape.id" :clue="getterEscape.clue" />
     </div>
-
-    
-
-
-
-
-
-
+  </div>
 </template>
 
 <style scoped>
 .background-title {
-    margin-bottom: 20px;
-    background: #FF4702;
+  margin-bottom: 20px;
+  background: #ff4702;
 }
 
 .backgroud-color {
-    background: rgba(0, 0, 0, 0.8);
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.45);
+  background: rgba(0, 0, 0, 0.8);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.45);
 }
 
 .text-center {
-    color: aliceblue;
+  color: aliceblue;
 }
 </style>
